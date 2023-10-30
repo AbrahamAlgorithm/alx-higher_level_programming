6ebdcefb76af5e27eb1749ad8bc6e6700e2f7fc3
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-10-31 00:43:05 +0100

    Linked List Cycle

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,1 +1,28 @@
-i
+#include "lists.h"
+/**
+ * check_cycle - check if linked list is cyclic
+ * @list: the linked list
+ * Return: 0 on False and 1 if true
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *current = list;
+
+	if (current->next != NULL)
+	{
+		current = current->next;
+	}
+	else
+	{
+		return (0);
+	}
+	while (current != list)
+	{
+		if (current->next == NULL)
+		{
+			return (0);
+		}
+		current = current->next;
+	}
+	return (1);
+}
