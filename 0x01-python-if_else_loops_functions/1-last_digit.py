41edf2e0112fc2f8a39c1ef431a91a23fd71a798
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-10-31 23:13:45 +0100

    the kast desketer

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+n = 0
+if number < 0:
+    n = -(-number % 10)
+else:
+    n = number % 10
+print("Last digit of {} is {} and is ".format(number, n), end="")
+if n > 5:
+    print("greater than 5")
+elif n == 0:
+    print('0')
+else:
+    print('less than 6 and not 0')``
