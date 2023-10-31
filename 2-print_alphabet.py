503f7b52154d43df09383ae95ab0b50e875d8a0c
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-10-31 23:19:11 +0100

    Update 2-print_alphabet.py

--- 2-print_alphabet.py
+++ 2-print_alphabet.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-import random
-number = random.randint(-10000, 10000)
-n = 0
-if number < 0:
-    n = -(-number % 10)
-else:
-    n = number % 10
-print("Last digit of {} is {} and is ".format(number, n), end="")
-if n > 5:
-    print("greater than 5")
-elif n == 0:
-    print('0')
-else:
-    print('less than 6 and not 0')
+for i in range(97, 123):
+    print("{}".format(chr(i)), end="")
