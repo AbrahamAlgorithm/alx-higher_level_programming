aa7b0aac4e9785062789030d7cdc1ae166ad54de
AbrahamAlgorithm <nightscholar0@gmail.com>
2023-10-31 23:02:05 +0100

    Positive anything is better than negative nothing

---
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -0,0 +1,8 @@
+mport random
+number = random.randint(-10, 10)
+if number > 0:
+        print(f"{number:d} is positive")
+    elif number == 0:
+        print(f'{number:d} is zero')489
+        else:
+            print(if'{number:d} is negative')
