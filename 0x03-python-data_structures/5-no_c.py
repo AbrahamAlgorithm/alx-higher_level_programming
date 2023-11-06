97a42a392ad7d09cb82fe6f7a433152cf4426eb5
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-11-06 13:58:54 +0100

    Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_string = ""
+    """return string without c and C
+
+    Args:
+        my_string: the string
+
+    Returns:
+        a new string without c and C
+    """
+    new_string = ''
     for i in range(len(my_string)):
         if my_string[i] not in 'cC':
             new_string += my_string[i]
-        return new_string
+    return (new_string)
