723368ea60853a506274f331501d599967786102
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-11-06 23:28:58 +0100

    Update 7-main.py

--- 0x03-python-data_structures/7-main.py
+++ 0x03-python-data_structures/7-main.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-multiple_returns = __import__('8-multiple_returns').multiple_returns
+add_tuple = __import__('7-add_tuple').add_tuple
 
-sentence = "At school, I learnt C!"
-length, first = multiple_returns(sentence)
-print("Length: {:d} - First character: {}".format(length, first))
+tuple_a = (1, 89)
+tuple_b = (88, 11)
+new_tuple = add_tuple(tuple_a, tuple_b)
+print(new_tuple)
+
+print(add_tuple(tuple_a, (1, )))
+print(add_tuple(tuple_a, ()))
