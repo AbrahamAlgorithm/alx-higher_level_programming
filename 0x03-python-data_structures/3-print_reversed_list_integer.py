6f77c2e2463ce41af215a064c08eac98270db601
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-11-06 12:51:18 +0100

    3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,21 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for item in reversed(my_list):
-        print("{:d}".format(item))
+    """Print list in reverse order
+
+    Args:
+        my_list: the list to reverse
+
+    Returns:
+        Null
+    """
+    if my_list is None:
+        return ('')
+    if len(my_list) == 0:
+        return ('')
+    for i in range(int(len(my_list) / 2)):
+        ldx = len(my_list) - i - 1
+        elem = my_list[ldx]
+        my_list[ldx] = my_list[i]
+        my_list[i] = elem
+    for i in my_list:
+        print("{:d}".format(i))
