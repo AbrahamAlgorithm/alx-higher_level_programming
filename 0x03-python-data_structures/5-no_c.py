e62a8366bf11100cff639eb392fe9fc9f3797516
AbrahamAlgorithm <nightscholar0@gmail.com>
2023-11-06 13:50:29 +0100

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for i in range(len(my_string)):
+        if my_string[i] not in 'cC':
+            new_string += my_string[i]
+        return new_string
