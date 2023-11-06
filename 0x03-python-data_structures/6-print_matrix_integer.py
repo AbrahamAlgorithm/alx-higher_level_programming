9b26c04a32e9a51c66984faf117d609fb95cabf5
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-11-07 00:03:00 +0100

    Update 6-print_matrix_integer.py

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    """print elements in a list of lists in matrix form
-
-    Args:
-        matrix: the element that contain list of list
-
-    Returns:
-        NULL
-    """
-    if len(matrix) == 0 or matrix is None:
-        return (None)
+    """ Function that prints a matrix of integers """
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
-            if j == len(matrix[i]) - 1:
-                print("{:d}".format(matrix[i][j]))
-            else:
-                print("{:d}".format(matrix[i][j]), end=' ')
+            if j != 0:
+                print(" ", end='')
+            print("{:d}".format(matrix[i][j]), end='')
+        print()
