7b384d3e5b7c2eb19c7a49540200f6d27fba05c7
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-11-09 00:44:44 +0100

    Search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    """search a list and replace it with replace
+
+    Args:
+        my_list: the list to search through
+        search: the element to search for
+        replace: the elemnt to replace it
+
+    Returns:
+        the new list with replace element
+    """
+    new_list = []
     if my_list is None:
         return (None)
     if len(my_list) == 0:
@@ -9,4 +20,4 @@ def search_replace(my_list, search, replace):
             new_list.append(replace)
         else:
             new_list.append(i)
-        return (new_list)
+    return (new_list)
