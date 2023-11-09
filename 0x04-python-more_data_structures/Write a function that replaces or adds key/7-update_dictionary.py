00f3af2fce566e5166b24f918ef925551957a678
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-11-09 01:01:06 +0100

    Update_dictionary

---
+++ 0x04-python-more_data_structures/Write a function that replaces or adds key/7-update_dictionary.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """replace a value in a dictionary
+
+    Args:
+        a_dictionary: the dictionary
+        key: the key
+        value: the value
+
+    Returns:
+    """
+    a_dictionary[key] = value
+    return (a_dictionary)
