038d52f73e5ea144d701563443e821936e221244
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2023-11-28 02:07:42 +0100

    N queens

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -4,34 +4,21 @@ import sys
 def is_safe(board, row, col, n):
     # Check if there is a queen in the same row on the left side
     for i in range(col):
-        if board[row][i] == 1:
+        if board[i] == row or \
+           board[i] == row - col + i or \
+           board[i] == row + col - i:
             return False
-
-    # Check upper diagonal on the left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    # Check lower diagonal on the left side
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
     return True
 
 def solve_nqueens_util(board, col, n):
-    if col >= n:
+    if col == n:
         print_solution(board, n)
-        return True
+        return
 
-    res = False
     for i in range(n):
         if is_safe(board, i, col, n):
-            board[i][col] = 1
-            res = solve_nqueens_util(board, col + 1, n) or res
-            board[i][col] = 0  # Backtrack if placing a queen at (i, col) doesn't lead to a solution
-
-    return res
+            board[col] = i
+            solve_nqueens_util(board, col + 1, n)
 
 def solve_nqueens(n):
     if not isinstance(n, int):
@@ -42,17 +29,12 @@ def solve_nqueens(n):
         print("N must be at least 4")
         sys.exit(1)
 
-    board = [[0 for _ in range(n)] for _ in range(n)]
-
-    if not solve_nqueens_util(board, 0, n):
-        print("No solution found")
+    board = [-1 for _ in range(n)]
+    solve_nqueens_util(board, 0, n)
 
 def print_solution(board, n):
-    for i in range(n):
-        for j in range(n):
-            print(board[i][j], end=" ")
-        print()
-    print()
+    solution = [[i, board[i]] for i in range(n)]
+    print(solution)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
