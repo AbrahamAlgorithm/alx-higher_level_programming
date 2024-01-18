f61389a84f52c1bf933a9bcf221d98322eae3133
Abraham Algorithm <101641878+AbrahamAlgorithm@users.noreply.github.com>
2024-01-18 23:39:07 +0100

    Rotten tomatoes

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,7 +1,7 @@
 -- Importing the database
-SELECT tv_shows.title, SUM(tv_show_ratings.rating) AS rating_sum
-FROM hbtn_0d_tvshows_rate.tv_shows
-JOIN hbtn_0d_tvshows_rate.tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
-GROUP BY tv_shows.id
-ORDER BY rating_sum DESC;
-
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS 'rating'
+      FROM tv_shows
+INNER JOIN tv_show_ratings
+        ON tv_shows.id = tv_show_ratings.show_id
+  GROUP BY title
+  ORDER BY rating DESC;
