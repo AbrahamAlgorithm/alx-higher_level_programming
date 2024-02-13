69b4c2bd554513531088c13766eb4debbbe27418
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-02-13 03:05:10 +0100

    Object

---
+++ 0x12-javascript-warm_up/12-object.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+
+myObject.value = 89; // Update value property
+
+console.log(myObject);
