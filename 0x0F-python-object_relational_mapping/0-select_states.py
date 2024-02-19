8f9e23445190863c1735e64b82d3bb24dd619152
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-02-19 02:43:59 +0100

    Get all states

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa """
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM `states`")
+    for i in cur.fetchall():
+        print(i)
+    cur.close()
+    db.close()
