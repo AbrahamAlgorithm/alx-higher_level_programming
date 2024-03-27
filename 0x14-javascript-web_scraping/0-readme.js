df5fb672162c2f78c2be8dbf23e522b43f993245
AbrahamAlgorithm <nightscholar0@gmail.com>
2024-03-27 17:35:51 +0100

    Rewadme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const filePath = process.argv[2];
-
-fs.readFile(filePath, 'utf-8', (err, data) => {
-	if (err) console.error(err);
-	console.log(data);
-}};
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  if (err) console.log(err);
+  console.log(data);
+});
